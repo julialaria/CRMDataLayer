4c72a6d570e619ca6230bbb729ad92b4016c0606
ester-naranjo-rodrigo <esthernaran@gmail.com>
2021-02-02 18:58:31 +0100

    CommandLine MainMethods implementation

--- src/main/java/com/ironhack/CRMDateLayer/CrmDateLayerApplication.java
+++ src/main/java/com/ironhack/CRMDateLayer/CrmDateLayerApplication.java
@@ -1,8 +1,11 @@
 package com.ironhack.CRMDateLayer;
 
 import com.ironhack.CRMDateLayer.model.SalesRep;
-import org.springframework.boot.SpringApplication;
+import com.ironhack.CRMDateLayer.repository.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class CrmDateLayerApplication {
@@ -13,4 +16,21 @@ public class CrmDateLayerApplication {
 
 	}
 
+	@Component
+	public class MyRunner implements CommandLineRunner{
+
+		@Autowired
+		private SalesRepRepository salesRepRepository;
+
+		@Override
+		public void run(String... args) throws Exception {
+
+			salesRepRepository.deleteAll();
+			salesRepRepository.save(new SalesRep("James"));
+			salesRepRepository.save(new SalesRep("Sara"));
+			salesRepRepository.save(new SalesRep("Michael"));
+			salesRepRepository.save(new SalesRep("Julia"));
+
+		}
+	}
 }
