ca30079078bc44f9ae93548fad713ee201031f82
Nerea Garcia <nereitagp12@gmail.com>
2021-02-04 22:06:09 +0100

    Some improvements

--- src/main/java/com/ironhack/CRMDateLayer/CrmDateLayerApplication.java
+++ src/main/java/com/ironhack/CRMDateLayer/CrmDateLayerApplication.java
@@ -3,13 +3,10 @@ package com.ironhack.CRMDateLayer;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class CrmDateLayerApplication {
-
     public static void main(String[] args) {
-       // SpringApplication.run(CrmDateLayerApplication.class, args);
         ConfigurableApplicationContext context = SpringApplication.run(CrmDateLayerApplication.class);
     }
 }
